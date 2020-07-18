f241c8449cc3c4cadc07529679754a869594f7f6
Brody Rethy <56841917+brodyrethy@users.noreply.github.com>
2020-07-18 17:30:47 -0300

    Update godsays.py

--- godsays.py
+++ godsays.py
@@ -1,13 +1,13 @@
 import random, sys
 
 def main():
-    sys.stdout.write("===================SPEAK TO GOD===================\n")
+    words = []
+    sys.stdout.write("God says: ")
     with open('english.txt') as f:
         godWords = f.read().splitlines()
-    word = []
     for x in range(0, 31):
-        word = random.choice(godWords)
-        sys.stdout.write(word)
+        words = random.choice(godWords)
+        sys.stdout.write(words)
         sys.stdout.write(" ")
     sys.stdout.write(".")
 
