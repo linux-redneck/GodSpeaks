44c18a1460dbc118039cb63ddf719aa8fe9a602c
Brody Rethy <56841917+brodyrethy@users.noreply.github.com>
2020-07-19 10:26:56 -0300

    Update godspeaks.py

--- godspeaks.py
+++ godspeaks.py
@@ -2,13 +2,27 @@ import random, sys
 
 def main():
     words = []
-    sys.stdout.write("God says: ")
+
     with open('english.txt') as f:
         godWords = f.read().splitlines()
-    for x in range(0, 31):
-        words = random.choice(godWords)
-        sys.stdout.write(words)
-        sys.stdout.write(" ")
-    sys.stdout.write(".")
+
+    while True: 
+        print("Insert 1 to quit")
+        print("Insert any other number to continue.\n")
+        try:
+            userInput = int(input("(> "))
+        except ValueError:
+            print("Try again")
+            main()
+        if (userInput == 1):
+            quit()
+        else:
+            sys.stdout.write("God says: ")
+            for x in range(0, 31):
+                words = random.choice(godWords)
+                sys.stdout.write(words)
+                sys.stdout.write(" ")
+
+            sys.stdout.write(".\n\n")
 
 main()
