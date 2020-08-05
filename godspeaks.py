6738c88b91ba33670acc1a8e64daf37102318a2f
Brody Rethy <brodyrethy@thewired.jp>
2020-08-05 01:13:15 -0300

    fg

--- godspeaks.py
+++ godspeaks.py
@@ -1,28 +1,20 @@
-import random, sys
+import random
 
 def main():
-    words = []
-
     with open('english.txt') as f:
         godWords = f.read().splitlines()
 
-    while True: 
-        print("Insert 1 to quit")
-        print("Insert any other number to continue.\n")
-        try:
-            userInput = int(input("(> "))
-        except ValueError:
-            print("Try again")
-            main()
-        if (userInput == 1):
-            quit()
-        else:
-            sys.stdout.write("God says: ")
-            for x in range(0, 31):
-                words = random.choice(godWords)
-                sys.stdout.write(words)
-                sys.stdout.write(" ")
+    print(end="God says... ")
 
-            sys.stdout.write(".\n\n")
+    #print random words from list godWords 31+1 times
+    for x in range(0, 30):
+        words = random.choice(godWords)
+        print(words, end=" ")
+
+    #print a random word from list godWords and append a period onto it
+    words = random.choice(godWords)
+    print(words + ".\n")
+
+    input("Press enter to continue")
 
 main()
